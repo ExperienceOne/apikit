de29320f921e914f1fee747014744a18d367abfd
Zoltan Giber <zoltan.giber@arabesque.com>
2021-10-27 14:21:41 +0100

    accept uuid V1 - V5

--- generator/xregex/xregex.go
+++ generator/xregex/xregex.go
@@ -1,7 +1,7 @@
 package xregex
 
 const (
-	UUIDGROUP  string = "([a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12})?"
+	UUIDGROUP  string = "([a-f0-9]{8}-[a-f0-9]{4}-[1-5]{1}[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12})?"
 	URLGROUP   string = `(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?`
 	REGEXEnd   string = "$"
 	REGEXBegin string = "^"
